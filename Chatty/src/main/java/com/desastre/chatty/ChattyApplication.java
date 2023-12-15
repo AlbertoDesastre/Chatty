96d66b1c076c19a389ca0f1c32bbd653e972766d
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-12-15 16:28:39 +0100

    added method in UserRepository to do queries in plain english

--- Chatty/src/main/java/com/desastre/chatty/ChattyApplication.java
+++ Chatty/src/main/java/com/desastre/chatty/ChattyApplication.java
@@ -24,13 +24,13 @@ public class ChattyApplication {
 
 	/**
 
-CAREFUL! If the class you are using for querying with MongoDB doesn't have an empty constructor available it will throw an exception. Here it's why:
-
-The need for a no-argument constructor in classes mapped by Spring Data MongoDB is related to how the conversion between MongoDB documents and Java instances is performed. Spring Data MongoDB uses reflection to instantiate objects of mapped classes from documents retrieved from the database.
-
-In Java, each class has a default (no-argument) constructor automatically provided if another constructor is not explicitly defined. When you define a constructor in your class, especially a constructor with arguments, the default constructor is no longer automatically provided. In other words, if you define any constructor in your class, Java will not automatically provide a default (no-argument) constructor.
-
-When Spring Data MongoDB retrieves documents from the database, it needs to be able to instantiate objects of your mapped classes. If there is no no-argument constructor available, reflection cannot instantiate the class without knowing the necessary arguments. Therefore, to facilitate the instantiation of mapped classes, Spring Data MongoDB requires a no-argument constructor.
+	CAREFUL! If the class you are using for querying with MongoDB doesn't have an empty constructor available it will throw an exception. Here it's why:
+	
+	The need for a no-argument constructor in classes mapped by Spring Data MongoDB is related to how the conversion between MongoDB documents and Java instances is performed. Spring Data MongoDB uses reflection to instantiate objects of mapped classes from documents retrieved from the database.
+	
+	In Java, each class has a default (no-argument) constructor automatically provided if another constructor is not explicitly defined. When you define a constructor in your class, especially a constructor with arguments, the default constructor is no longer automatically provided. In other words, if you define any constructor in your class, Java will not automatically provide a default (no-argument) constructor.
+	
+	When Spring Data MongoDB retrieves documents from the database, it needs to be able to instantiate objects of your mapped classes. If there is no no-argument constructor available, reflection cannot instantiate the class without knowing the necessary arguments. Therefore, to facilitate the instantiation of mapped classes, Spring Data MongoDB requires a no-argument constructor.
 	 */
 	
 	
@@ -38,31 +38,43 @@ When Spring Data MongoDB retrieves documents from the database, it needs to be a
 	CommandLineRunner runner(UserRepository userRepository, MongoTemplate mongoTemplate) { // Executes a command
 		
 		return args -> {
-			String email = "email@email.com";
-			User user = new User(email,
-					"my avatar", 
-					"crazy description", 
-					"token", 
-					"123456password");
 			
-			Query query = new Query();
-			query.addCriteria(Criteria.where("email").is(email));
-			 
-            List<User> users = mongoTemplate.find(query, User.class);
+			// insertStudentMongoTemplateAndQuery(userRepository, mongoTemplate);
 			
-            if(users.size() > 1) {
-            	throw new IllegalStateException("There are multiple users with the same email, look: ");
-            }
-            
-            if(users.isEmpty()) {
-            	userRepository.insert(user);
-            	System.out.println("User created.");
-            } else {
-            	throw new IllegalStateException("Can't create the user because already exists.");
-            }
+			userRepository.findByEmail("email@email.com").ifPresentOrElse( user -> {
+				System.out.println("This user was found --> " + user);
+			}, () -> {
+				System.out.println("The user wasn't found.");
+			});;
+			
             
 		};
 
+	}
+
+	private void insertStudentMongoTemplateAndQuery(UserRepository userRepository, MongoTemplate mongoTemplate) {
+		String email = "email@email.com";
+		User user = new User(email,
+				"my avatar", 
+				"crazy description", 
+				"token", 
+				"123456password");
+		
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(email));
+		 
+		List<User> users = mongoTemplate.find(query, User.class);
+		
+		if(users.size() > 1) {
+			throw new IllegalStateException("There are multiple users with the same email, look: ");
+		}
+		
+		if(users.isEmpty()) {
+			userRepository.insert(user);
+			System.out.println("User created.");
+		} else {
+			throw new IllegalStateException("Can't create the user because already exists.");
+		}
 	}
 
 }
--- Chatty/src/main/java/com/desastre/chatty/repositories/UserRepository.java
+++ Chatty/src/main/java/com/desastre/chatty/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.desastre.chatty.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.desastre.chatty.models.User;
 
-public interface UserRepository extends MongoRepository<User, String>{
 
-	
+public interface UserRepository extends MongoRepository<User, String>{
+	Optional<User> findByEmail(String email);
 	
 }
